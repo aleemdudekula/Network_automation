0dfc392a85a89b5e90bfacd6196235f19507fe36
Aleem dudekula <40751865+aleemdudekula@users.noreply.github.com>
2026-08-21 14:30:50 +0530

    Add script to send configuration from file using Netmiko

---
+++ projects/basic_lab/Netmiko/send_config_from_file.py
@@ -0,0 +1,13 @@
+from netmiko import ConnectHandler
+device_details = {
+    "device_type": "cisco_ios",
+    "host" : "192.168.79.32",
+    "username": "aleem",
+    "password" : "Condor@8",
+    "secret" : "cisco123"
+}
+net_connect = ConnectHandler(**device_details)
+net_connect.enable()
+
+output = net_connect.send_config_from_file("cmds.txt")
+print(output)
