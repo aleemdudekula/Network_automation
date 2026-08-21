c0a5535c724759e6993d007e7011d04e89b9352f
Aleem dudekula <40751865+aleemdudekula@users.noreply.github.com>
2026-08-21 14:40:24 +0530

    Clean up output comments in send_multiline.py
    Removed output comments from send_multiline.py.

--- projects/basic_lab/Netmiko/send_multiline.py
+++ projects/basic_lab/Netmiko/send_multiline.py
@@ -12,4 +12,19 @@ net_connect.enable()
 output = net_connect.send_multiline(["show ip interface brief"],
                                     ["show running-config"])
 print(output)
-#output
+# Please remove output before run above program 
+output
+(.network-aut) root@ubuntu:~/Network_automation/projects/basic_lab# python3 config.py
+show ip interface brief
+Interface                  IP-Address      OK? Method Status                Protocol
+FastEthernet0/0            unassigned      YES NVRAM  administratively down down    
+FastEthernet1/0            unassigned      YES NVRAM  administratively down down    
+FastEthernet2/0            unassigned      YES NVRAM  administratively down down    
+FastEthernet3/0            unassigned      YES NVRAM  administratively down down    
+FastEthernet4/0            unassigned      YES NVRAM  administratively down down    
+FastEthernet5/0            unassigned      YES NVRAM  administratively down down    
+Ethernet6/0                192.168.1.1     YES manual up                    up      
+Ethernet6/1                192.168.79.32   YES manual up                    up      
+Ethernet6/2                unassigned      YES NVRAM  administratively down down    
+Ethernet6/3                unassigned      YES NVRAM  administratively down down    
+R2#
