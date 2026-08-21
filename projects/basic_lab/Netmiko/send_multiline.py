d1feb456e61dee016d358bf7abe6dafd53d7ebb8
Aleem dudekula <40751865+aleemdudekula@users.noreply.github.com>
2026-08-21 14:38:04 +0530

    Add script to send multiple commands using Netmiko

---
+++ projects/basic_lab/Netmiko/send_multiline.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+device_details = {
+    "device_type": "cisco_ios",
+    "host" : "192.168.79.32",
+    "username": "aleem",
+    "password" : "Condor@8",
+    "secret" : "cisco123"
+}
+net_connect = ConnectHandler(**device_details)
+net_connect.enable()
+
+output = net_connect.send_multiline(["show ip interface brief"],
+                                    ["show running-config"])
+print(output)
