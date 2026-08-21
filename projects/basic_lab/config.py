3f4fdfc53a0d6aa7f2454ef6afd45a2912a1294a
aleemdudekula <dudekulaaleem12@gmail.com>
2026-08-21 08:50:23 +0000

    doing multiple cmds excution on different device on remotely

--- projects/basic_lab/config.py
+++ projects/basic_lab/config.py
@@ -1,7 +1,7 @@
 from netmiko import ConnectHandler
 device_details = {
     "device_type": "cisco_ios",
-    "host" : "192.168.79.10",
+    "host" : "192.168.79.32",
     "username": "aleem",
     "password" : "Condor@8",
     "secret" : "cisco123"
@@ -9,8 +9,6 @@ device_details = {
 net_connect = ConnectHandler(**device_details)
 net_connect.enable()
 
-output = net_connect.send_command("show ip int brief")
+output = net_connect.send_config_from_file("cmds.txt")
+output = net_connect.send_command("show ip interface brief")
 print(output)
-
-net_connect.disconnect()
-
